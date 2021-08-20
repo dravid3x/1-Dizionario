c78f8b89a8aa5b2c7e0c5fbcedf5e0d6f8d1aff9
dravid3x <39882540+dravid3x@users.noreply.github.com>
2021-08-20 16:11:43 +0200

    Concluso, aggiunta versione + veloce

--- 1) Dizionario/1) Dizionario.cpp
+++ 1) Dizionario/1) Dizionario.cpp
@@ -12,11 +12,10 @@ using namespace std;
 #include <time.h>
 #pragma execution_character_set( "utf-8" )
 
-void inserimentoFrase(), dividiFrase(string frase, char divisore, string* destinazione);
+void inserimentoFrase(), dividiFrase(string frase, char divisore, string destinazione[]), controlloVeloce(), controlloLento();
 bool isAnagramma(string parola1, string parola2);
 
 const int dim = 1000;
-int logic_dim;
 ifstream dizionario("dizionario.txt");
 string fraseInserita;
 string fraseInseritaArr[dim];
@@ -27,40 +26,94 @@ int main()
 	inserimentoFrase();
 	dividiFrase(fraseInserita, ' ', fraseInseritaArr);
 	cout << "\n";
+	bool finito = false;
+	while (!finito) {
+		system("CLS");
+		cout << "Frase inserita: " << fraseInserita << "\n\n";
+
+		cout << "Scegliere l'algoritmo da utilizzare\n\n";
+		cout << "1) molti cicli dizionario, 1 ciclo parole (lento ca. 3 sec a parola)\n";
+		cout << "2) 1 ciclo dizionario, molti cicli parole (veloce)\n";
+		cout << "-1) Exit\n";
+		cout << "\n\n>> ";
+		string scelta;
+		cin >> scelta;
+		int numeroInt;
+		try
+		{
+			numeroInt = stoi(scelta);
+			switch (numeroInt)
+			{
+			case 1:
+				controlloLento();
+				break;
+			case 2:
+				controlloVeloce();
+				break;
+			case -1:
+				finito = true;
+				break;
+			default:
+				system("CLS");
+				break;
+			}
+		}
+		catch (const std::exception& exc)
+		{
+
+		}
+	}
+	return 0;
+}
+
+void controlloVeloce() {
+	//Per ogni parola della frase inserita eseguo il confronto con ogni elemento all'interno del dizionario
+	//Scorro solamente 1 volta il dizionario ma molte volte le parole inserite
+	clock_t start = clock();
+	int pos = 0, nAnagrammi = 0;
+	string parola = fraseInseritaArr[pos];
+	while (!dizionario.eof()) {
+		string linea;
+		dizionario >> linea;
+		while (parola != "") {
+			if (isAnagramma(parola, linea)) cout << "Anagramma n. " << nAnagrammi++ << " : Parola inserita -> " << parola << " Parola dizionario -> " << linea << "\n";
+			pos++;
+			parola = fraseInseritaArr[pos];
+		}
+		pos = 0;
+		parola = fraseInseritaArr[pos];
+	}
+	clock_t end = clock();
+	dizionario.clear();
+	dizionario.seekg(0);	//Reimposto la posizione nel file dizionario
+	cout << "\nTempo di esecuzione = secondi " << (double)(end - start) / CLOCKS_PER_SEC << "\n";
+	cout << "\n";
+	system("PAUSE");
+}
+
+void controlloLento() {
 	int pos = 0, nAnagrammi = 0;
 	string parola = fraseInseritaArr[pos];;
 	//Per ogni parola della frase inserita eseguo il confronto con ogni elemento all'interno del dizionario
 	clock_t start = clock();
 	while (parola != "") {
 		dizionario.clear();
-		dizionario.seekg(0);
+		dizionario.seekg(0);	//Reimposto la posizione nel file dizionario
 		cout << "Parola analizzata: " << parola << "\n";
 		while (!dizionario.eof()) {
 			string linea;
 			dizionario >> linea;
-			if(isAnagramma(parola, linea)) cout << "Anagramma n. " << nAnagrammi++ << " : " << linea << "\n";
+			if (isAnagramma(parola, linea)) cout << "Anagramma n. " << nAnagrammi++ << " : " << linea << "\n";
 		}
 		pos++;
 		parola = fraseInseritaArr[pos];
 	}
 	clock_t end = clock();
+	dizionario.clear();
+	dizionario.seekg(0);	//Reimposto la posizione nel file dizionario
 	cout << "\nTempo di esecuzione = secondi " << (double)(end - start) / CLOCKS_PER_SEC << "\n";
 	cout << "\n";
 	system("PAUSE");
-
-
-
-
-	/*
-	while (!dizionario.eof()) {
-		string linea;
-		dizionario >> linea;
-		cout << linea << "\n";
-	}
-	cout << "\n";
-	dizionario.close();
-	return 0;
-	*/
 }
 
 void inserimentoFrase() {
